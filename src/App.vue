cd8e7295180f1bb54fe5cf7e8e23b2bd7f430282
Pablo Souza <pvrsouza@gmail.com>
2020-01-23 06:11:07 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <app-contador></app-contador>
+    <app-contador></app-contador>
+    <app-contador></app-contador>
+    <app-contador></app-contador>
+    <app-contador></app-contador>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  el: "app",
+  name: "contador",
+  data(){
+    return {
+      contador: 0,
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import Contador from './components/Contador.vue'
+
+Vue.config.productionTip = false
+
+//Components globais
+Vue.component('app-contador', Contador)
+
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
